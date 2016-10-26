f0f2a2775f5622fa809d30c87bee81819356fa3f
Gabriel Dahia <gdahia@gmail.com>
2016-10-26 19:32:53 -0300

    new way to plot stuff

--- validation/recognition/nth_rank.R
+++ validation/recognition/nth_rank.R
@@ -1,27 +1,21 @@
 #!/usr/bin/env Rscript
-args = commandArgs(trailingOnly=TRUE)
-rank <- scan("fingerprint/true/nthrank.txt")
 pdf(file="nthrank.pdf")
-par(mgp=c(2.2,0.45,0), tcl=-0.4, mar=c(3.3,3.3,3.3,1.1))
-plot(c(1:50), rank, xlab="N", ylab="Rank-N", col="blue", pch=16, type="l", xaxs="i", yaxs="i")
-par(new = TRUE)
-rank <- scan("fingerprint/automatic/nthrank.txt")
-plot(c(1:50), rank, type='l', pch=16, col="red", axes = FALSE, ann = FALSE, xaxs="i", yaxs="i")
-par(new = TRUE)
-rank <- scan("iris/solo/nthrank.txt")
-plot(c(1:50), rank, type='l', pch=16, col="darkgreen", axes = FALSE, ann = FALSE, xaxs="i", yaxs="i")
-# par(new = TRUE)
-# rank <- scan("iris/and_fingerprint/nthrank.txt")
-# plot(c(1:50), rank, type='l', pch=16, col="green", axes = FALSE, ann = FALSE, xaxs="i", yaxs="i")
+
+par(mgp = c(2.2,0.45,0), tcl = -0.4, mar = c(3.3,3.3,3.3,1.1))
+plot(c(1:50), c(1:50), xlab = "N", ylab = "Rank-N", type="n", xlim = range(1:50), ylim = range(1:50), xaxs="i", yaxs="i")
 grid(col = "lightgray", lty = "dotted", lwd = par("lwd"), equilogs = TRUE)
 legend("bottomright", inset=.05, cex = 0.75, c("Impressão digital - detecção manual", "Impressão digital - detecção automática", "Iris - detecção manual", "Iris + Impressão digital - manual"), lty=c(1,1,1,1), lwd=c(2,2,2,2), col=c("blue", "red", "darkgreen", "green"), bg="grey96")
+
 par(new = TRUE)
 rank <- scan("fingerprint/true/nthrank.txt")
-plot(c(1:50), rank, xlab="N", ylab="Rank-N", col="blue", pch=16, type="l", xaxs="i", yaxs="i")
+plot(c(1:50), rank, col="blue", pch=16, type="l", axes = FALSE, ann = FALSE, xaxs="i", yaxs="i", xlim = range(1:50), ylim = range(1:50))
+
 par(new = TRUE)
 rank <- scan("fingerprint/automatic/nthrank.txt")
-plot(c(1:50), rank, type='l', pch=16, col="red", axes = FALSE, ann = FALSE, xaxs="i", yaxs="i")
+plot(c(1:50), rank, type='l', pch=16, col="red", axes = FALSE, ann = FALSE, xaxs="i", yaxs="i", xlim = range(1:50), ylim = range(1:50))
+
 par(new = TRUE)
 rank <- scan("iris/solo/nthrank.txt")
-plot(c(1:50), rank, type='l', pch=16, col="darkgreen", axes = FALSE, ann = FALSE, xaxs="i", yaxs="i")
+plot(c(1:50), rank, type='l', pch=16, col="darkgreen", axes = FALSE, ann = FALSE, xaxs="i", yaxs="i", xlim = range(1:50), ylim = range(1:50))
+
 dev.off()
