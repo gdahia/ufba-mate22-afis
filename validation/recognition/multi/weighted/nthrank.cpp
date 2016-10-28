5efc73a7d16d41167bbe1009863756c892769370
Gabriel Dahia <gdahia@gmail.com>
2016-10-28 09:28:48 -0300

    nthrank for weighted

---
+++ validation/recognition/multi/weighted/nthrank.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+unsigned int nth_rank(vector<pair<double, int>> & diffs, const unsigned int gen) {
+    sort(diffs.begin(), diffs.end());
+
+    queue<int> q;
+    bool appeared_class[51];
+    memset(appeared_class, false, sizeof appeared_class);
+    for (pair<double, int> p : diffs) {
+        if (p.second == gen) {
+            while (!q.empty()) {
+                unsigned int tobmarked = q.front();
+                q.pop();
+                appeared_class[tobmarked] = true;
+            }
+        }
+        q.push(p.second);
+    }
+
+    unsigned int rank = 0;
+    for (int i = 1; i <= 50; i++)
+        rank += appeared_class[i];
+
+    return rank;
+}
+
+int main(int argc, char ** argv) {
+    if (argc <= 1) {
+        cerr << "Error: pass the similarity/difference file as parameter" << endl;
+        return 1;
+    }
+
+    /* attempt to open file */
+    ifstream input(argv[1], ios::in);
+    if (!input.is_open()) {
+        cerr << "Error: Unable to open file " << argv[1] << endl;
+        return 1;
+    }
+
+    /* read contents and store accordingly */
+    vector<pair<double, int>> diffs[51][6];
+    int sub1, reg1, sub2, reg2;
+    double diff;
+    while (input >> sub1 >> reg1 >> sub2 >> reg2 >> diff)
+        diffs[sub1][reg1].emplace_back(diff, sub2);
+
+    /* compute ranks */
+    unsigned int ranks[51];
+    memset(ranks, 0, sizeof ranks);
+
+    for (int i = 1; i <= 50; i++)
+        for (int j = 1; j <= 5; j++)
+            ranks[nth_rank(diffs[i][j], i)]++;
+
+    /* output ranks */
+    int rank = 0;
+    for (int i = 1; i <= 50; i++) {
+        rank += ranks[i];
+        cout << rank << endl;
+    }
+}
--- validation/recognition/nth_rank.R
+++ validation/recognition/nth_rank.R
@@ -4,7 +4,7 @@ pdf(file="nthrank.pdf")
 par(mgp = c(2.2,0.45,0), tcl = -0.4, mar = c(3.3,3.3,3.3,1.1))
 plot(c(1:50), c(1:50), xlab = "N", ylab = "Rank-N", type="n", xlim = range(1:50), ylim = range(1:250), xaxs="i", yaxs="i")
 grid(col = "lightgray", lty = "dotted", lwd = par("lwd"), equilogs = TRUE)
-legend("bottomright", inset=.02, cex = 0.75, c("Impressão digital - detecção manual", "Impressão digital - detecção automática", "Iris - detecção manual", "Iris + Impressão digital - manual"), lty=c(1,1,1,1), lwd=c(2,2,2,2), col=c("blue", "red", "darkgreen", "green"), bg="grey96")
+legend("bottomright", inset=.02, cex = 0.75, c("Impressão digital - detecção manual", "Impressão digital - detecção automática", "Íris - detecção manual", "Íris + Impressão digital - Probabilístico", "Íris + Impressão digital - Ponderado", "Aleatório"), lty=c(1,1,1,1,1,5), lwd=c(2,2,2,2,2,2), col=c("blue", "red", "darkgreen", "purple", "green", "black"), bg="grey96")
 
 par(new = TRUE)
 rank <- scan("fingerprint/true/nthrank.txt")
@@ -19,7 +19,11 @@ rank <- scan("iris/solo/nthrank.txt")
 plot(c(1:50), rank, type='l', pch=16, col="darkgreen", axes = FALSE, ann = FALSE, xaxs="i", yaxs="i", xlim = range(1:50), ylim = range(1:250))
 
 par(new = TRUE)
-rank <- scan("iris/and_fingerprint/nthrank.txt")
+rank <- scan("multi/prob/nthrank.txt")
+plot(c(1:50), rank, type='l', pch=16, col="purple", axes = FALSE, ann = FALSE, xaxs="i", yaxs="i", xlim = range(1:50), ylim = range(1:250))
+
+par(new = TRUE)
+rank <- scan("multi/weighted/nthrank.txt")
 plot(c(1:50), rank, type='l', pch=16, col="green", axes = FALSE, ann = FALSE, xaxs="i", yaxs="i", xlim = range(1:50), ylim = range(1:250))
 
 dev.off()
