e3de6caf4a066969f588e4794ba34f406040464e
Gabriel Dahia <gdahia@gmail.com>
2016-10-28 11:44:28 -0300

    removed random from nthrank

--- validation/recognition/nth_rank.R
+++ validation/recognition/nth_rank.R
@@ -4,7 +4,7 @@ pdf(file="nthrank.pdf")
 par(mgp = c(2.2,0.45,0), tcl = -0.4, mar = c(3.3,3.3,3.3,1.1))
 plot(c(1:50), c(1:50), xlab = "N", ylab = "Rank-N", type="n", xlim = range(1:50), ylim = range(1:250), xaxs="i", yaxs="i")
 grid(col = "lightgray", lty = "dotted", lwd = par("lwd"), equilogs = TRUE)
-legend("bottomright", inset=.02, cex = 0.75, c("Impressão digital - detecção manual", "Impressão digital - detecção automática", "Íris - detecção manual", "Íris + Impressão digital - Probabilístico", "Íris + Impressão digital - Ponderado", "Aleatório"), lty=c(1,1,1,1,1,5), lwd=c(2,2,2,2,2,2), col=c("blue", "red", "darkgreen", "purple", "green", "black"), bg="grey96")
+legend("bottomright", inset=.02, cex = 0.75, c("Impressão digital - detecção manual", "Impressão digital - detecção automática", "Íris - detecção manual", "Íris + Impressão digital - Probabilístico", "Íris + Impressão digital - Ponderado"), lty=c(1,1,1,1,1), lwd=c(2,2,2,2,2), col=c("blue", "red", "darkgreen", "purple", "green"), bg="grey96")
 
 par(new = TRUE)
 rank <- scan("fingerprint/true/nthrank.txt")
